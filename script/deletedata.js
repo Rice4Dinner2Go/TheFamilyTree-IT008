5193edac25890bfeb1b708ae69f82dc5c8120265
BATLUONGNHAN <164552277+BATLUONGNHAN@users.noreply.github.com>
2024-12-12 21:46:18 +0700

    update delete data

--- script/deletedata.js
+++ script/deletedata.js
@@ -1,3 +1,4 @@
+import api from './api.js';
 async function delete_all_data ()
 {
     try {
@@ -16,6 +17,37 @@ async function delete_all_data ()
             try{
                 await api.deletePerson(person.id);
                 console.log(`Deleted person with ID: ${person.id}`);
+                const axios = require('axios');
+
+                let config = 
+                {
+                    method: 'delete',
+                    maxBodyLength: Infinity,
+                    url: `https://squid-app-7jubn.ondigitalocean.app/family/persons/${person.id}`,
+                    headers: 
+                    { 
+                        'accept': 'application/json', 
+                        'accept-language': 'vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5', 
+                        'origin': 'https://squid-app-7jubn.ondigitalocean.app', 
+                        'priority': 'u=1, i', 
+                        'referer': 'https://squid-app-7jubn.ondigitalocean.app/explorer/?fbclid=IwY2xjawHHxcVleHRuA2FlbQIxMAABHcLe2rx7O0jFYgeQv1USxFwYOGZa4IToI307eplVQIqevndFU0_zMWm_nw_aem_uxzaB0rDfEX6-17DitIxTA', 
+                        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"', 
+                        'sec-ch-ua-mobile': '?0', 
+                        'sec-ch-ua-platform': '"Windows"', 
+                        'sec-fetch-dest': 'empty', 
+                        'sec-fetch-mode': 'cors', 
+                        'sec-fetch-site': 'same-origin', 
+                        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
+                    }
+                };
+                axios.request(config)
+                .then((response) => {
+                console.log(JSON.stringify(response.data));
+                })
+                .catch((error) => {
+                console.log(error);
+                });
+
             } 
             catch (error)
             {
